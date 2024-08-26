3721aba72e3a1a64488bf07ed14c27cfa1fbd815
Lucas Stopinski da Silva <lucas.stopinski@tecpuc.com.br>
2024-08-26 20:26:33 -0300

    JavaScript e HTML
    JavaScript agora está funcionando e HTML está correto

--- Projetinho Inicial/index.js
+++ Projetinho Inicial/index.js
@@ -1,3 +1,18 @@
+var express = require('express');
+var app = express();
+
+const port = 3000;
+
+app.use(express.static('./pages'));
+
+app.get('/hello', (req, res) => {
+    res.send('Hello, World! 123');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
 const form = document.getElementById("form");
 
 const username = document.getElementById("username")
---
+++ Projetinho Inicial/pages/js/funcoes.js
@@ -0,0 +1,86 @@
+const form = document.getElementById("form");
+
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const senhaConfirmacao = document.getElementById("senhaConfirmacao");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    checkForm();
+});
+
+function checkInputUsername(){
+    const usernameValue = username.value;
+
+    if (usernameValue === ""){
+        errorInput(username, "Campo obrigatório.");
+    } else {
+        const formItem = username.parentElement;
+        formItem.className = "form-content";
+    }
+}
+
+function checkInputEmail(){
+    const emailValue = email.value;
+
+    if (emailValue === ""){
+        errorInput(email, "Campo obrigatório.");
+    } else {
+        const formItem = email.parentElement;
+        formItem.className = "form-content";
+    }
+}
+
+function checkInputSenha(){
+    const senhaValue = senha.value;
+
+    if (senhaValue === ""){
+        errorInput(senha, "A senha é obrigatória.");
+    } else if (senhaValue.length < 8){
+        errorInput(senha, "A senha precisa ter no mínimo 8 caracteres.");
+    } else {
+        const formItem = senha.parentElement;
+        formItem.className = "form-content";
+    }
+}
+
+function checkInputSenhaConfirmacao(){
+    const senhaValue = senha.value;
+    const senhaConfirmacaoValue = senhaConfirmacao.value;
+
+    if (senhaConfirmacaoValue === ""){
+        errorInput(senhaConfirmacao, "A confirmação de senha é obrigatória.");
+    } else if (senhaConfirmacaoValue !== senhaValue){
+        errorInput(senhaConfirmacao, "As senhas não são iguais.");
+    } else {
+        const formItem = senhaConfirmacao.parentElement;
+        formItem.className = "form-content";
+    }
+}
+
+function checkForm(){
+    checkInputUsername();
+    checkInputEmail();
+    checkInputSenha();
+    checkInputSenhaConfirmacao();
+
+    const formItems = form.querySelectorAll(".form-content");
+
+    const isValid = [...formItems].every((item) => {
+        return item.className === "form-content";
+    });
+
+    if (isValid){
+        alert("Cadastro realizado com sucesso");
+    }
+}
+
+function errorInput(input, message){
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("span"); // Supondo que você use <span> para exibir a mensagem de erro
+
+    textMessage.innerText = message;
+
+    formItem.className = "form-content error";
+}
